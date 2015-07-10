a652870cdc6213e8128d0886cf6a30bb75e4f4a9
sam bassett <samson.bassett@gmail.com>
2015-07-10 00:44:03 -0700

    inventory screen debugging

--- src/main/java/com/lothrazar/samsinvcrafting/ContainerPlayerCrafting.java
+++ src/main/java/com/lothrazar/samsinvcrafting/ContainerPlayerCrafting.java
@@ -1,43 +1,102 @@
 package com.lothrazar.samsinvcrafting;
 
+import com.google.common.collect.Lists;
+
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.ContainerPlayer;
+import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.inventory.Slot;
+import net.minecraft.inventory.SlotCrafting;
+import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ContainerPlayerCrafting extends ContainerPlayer 
 {
 	private int craftingSize = 3;//2 in vanilla
 	public InventoryPlayerCrafting invo;
-	Slot[] crafting = new Slot[9];
+	private final EntityPlayer thePlayer;
+
 	public ContainerPlayerCrafting(InventoryPlayerCrafting playerInventory,	boolean localWorld, EntityPlayer player) 
 	{
 		super(playerInventory, localWorld, player);
 		System.out.println("ContainerPlayerCrafting constructor");
-		this.invo = playerInventory;
+		inventorySlots = Lists.newArrayList();
+		//this.isLocalWorld = localWorld;
+        this.thePlayer = player;
+		
+		craftMatrix = new InventoryCrafting(this, craftingSize, craftingSize);
+        this.addSlotToContainer(new SlotCrafting(playerInventory.player, this.craftMatrix, this.craftResult, 0, 144, 36));
+        int i;
+        int j;
 
-		//TODO: stuff like crafting = new Slot[9];
-		int i,j;
-		for (i = 0; i < craftingSize; ++i)
+        for (i = 0; i < craftingSize; ++i)
         {
             for (j = 0; j < craftingSize; ++j)
             {
-	            if(i == 2 || j == 2)
-	            {
-	
-	                System.out.println("new "+i+" "+j);
-	 
-	                this.addSlotToContainer(new Slot(this.craftMatrix, j + i * 2, 88 + j * 18, 26 + i * 18));
-	            }
-	            else
-	            {
-	            System.out.println("already in"+i+" "+j);
-	            
-	          
-	            }
+            	System.out.println("craftslot at "+i+" "+j);
+                this.addSlotToContainer(new Slot(this.craftMatrix, j + i * 2, 88 + j * 18, 26 + i * 18));
             }
         }
+
+        for (i = 0; i < 4; ++i)//the four armor slots
+        {
+            final int k = i;
+            this.addSlotToContainer(new Slot(playerInventory, playerInventory.getSizeInventory() - 1 - i, 8, 8 + i * 18)
+            {
+                private static final String __OBFID = "CL_00001755";
+                /**
+                 * Returns the maximum stack size for a given slot (usually the same as getInventoryStackLimit(), but 1
+                 * in the case of armor slots)
+                 */
+                public int getSlotStackLimit()
+                {
+                    return 1;
+                }
+                /**
+                 * Check if the stack is a valid item for this slot. Always true beside for the armor slots.
+                 */
+                public boolean isItemValid(ItemStack stack)
+                {
+                    if (stack == null) return false;
+                    return stack.getItem().isValidArmor(stack, k, thePlayer);
+                }
+                @SideOnly(Side.CLIENT)
+                public String getSlotTexture()
+                {
+                    return ItemArmor.EMPTY_SLOT_NAMES[k];
+                }
+            });
+        }
+
+        for (i = 0; i < 3; ++i)
+        {
+            for (j = 0; j < 9; ++j)
+            {
+                this.addSlotToContainer(new Slot(playerInventory, j + (i + 1) * 9, 8 + j * 18, 84 + i * 18));
+            }
+        }
+
+        for (i = 0; i < 9; ++i)
+        {
+            this.addSlotToContainer(new Slot(playerInventory, i, 8 + i * 18, 142));
+        }
+
+		this.invo = playerInventory;
+        this.onCraftMatrixChanged(this.craftMatrix);
+	
+		//TODO: stuff like crafting = new Slot[9];
+		/*	int i,j;
+		
+	    for (i = 0; i < craftingSize; ++i)
+        {
+            for (j = 0; j < craftingSize; ++j)
+            {
+                this.addSlotToContainer(new Slot(this.craftMatrix, j + i * 2, 88 + j * 18, 26 + i * 18));
+            }
+        }*/
         
 	}
 	@Override
--- src/main/java/com/lothrazar/samsinvcrafting/GuiInventoryCrafting.java
+++ src/main/java/com/lothrazar/samsinvcrafting/GuiInventoryCrafting.java
@@ -6,30 +6,53 @@ import net.minecraft.entity.player.EntityPlayer;
 public class GuiInventoryCrafting extends GuiInventory 
 {
 	ContainerPlayerCrafting container;
-	public GuiInventoryCrafting(EntityPlayer p_i1094_1_) 
+	public GuiInventoryCrafting(EntityPlayer p) 
 	{
-		super(p_i1094_1_);
+		super(p);
 		System.out.println("GuiInventoryCrafting constructor");
-		 this.container = ((p_i1094_1_.inventoryContainer instanceof ContainerPlayerCrafting) ? (ContainerPlayerCrafting)p_i1094_1_.inventoryContainer : null);
-	
-		 if (this.container == null)
-		 {
-
-			 ModInvCrafting.logger.log(org.apache.logging.log4j.Level.WARN,"Player GUI is null");
-			 
-			 if (p_i1094_1_.inventoryContainer != null)
+		
+		//InventoryContainer ic = p_i1094_1_.inventoryContainer;
+		
+		//java.lang.ClassCastException: net.minecraft.entity.player.InventoryPlayer cannot be cast to com.lothrazar.samsinvcrafting.InventoryPlayerCrafting
+		
+		//InventoryPlayerCrafting ipc = new InventoryPlayerCrafting(p);
+		// this.container = ((p_i1094_1_.inventoryContainer instanceof ContainerPlayerCrafting) ? (ContainerPlayerCrafting)p_i1094_1_.inventoryContainer : null);
+		try
+		{
+			 if(this.container == null)// || ic instanceof ContainerPlayerCrafting)
 			 {
-				 ModInvCrafting.logger.log(org.apache.logging.log4j.Level.WARN, "GUI name " + p_i1094_1_.inventoryContainer.getClass().getSimpleName() + "!", new IllegalArgumentException());
+				 System.out.println("container null, remaking");
+					//this.container = new ContainerPlayerCrafting((InventoryPlayerCrafting)p.inventory, p.worldObj.isRemote, p);
+				 this.container = new ContainerPlayerCrafting(new InventoryPlayerCrafting(p), p.worldObj.isRemote, p);
+	
+			 }
+		}
+		catch(Exception e)
+		{
+			// ModInvCrafting.logger.log(org.apache.logging.log4j.Level.WARN,"Player GUI is null?"+e.getMessage(),e);
+			 
+			 if (p.inventoryContainer != null)
+			 {
+				 ModInvCrafting.logger.log(org.apache.logging.log4j.Level.WARN, "GUI name " + p.inventoryContainer.getClass().getSimpleName() + "!", e);
 			 }
 			 else {
-				 ModInvCrafting.logger.log(org.apache.logging.log4j.Level.WARN, "Null container!", new NullPointerException());
+				 ModInvCrafting.logger.log(org.apache.logging.log4j.Level.WARN, "Null container!", e);
 			 }
 		 }
-	
+		
+		
 	}
 	protected void drawGuiContainerBackgroundLayer(float p_146976_1_, int p_146976_2_, int p_146976_3_)
 	{
+		//this should load texture, but fails and gives transparent
+		//was 
+		/*[23:08:44] [Client thread/WARN]: Failed to load texture: samsinvcrafting:assets/gui/inventory33.png
+java.io.FileNotFoundException: samsinvcrafting:assets/gui/inventory33.png*/
 		
-		  this.mc.getTextureManager().bindTexture(new net.minecraft.util.ResourceLocation(ModInvCrafting.MODID, "textures/gui/inventory33.png"));
+		
+		/*[23:11:01] [Client thread/WARN]: Failed to load texture: samsinvcrafting:textures/gui/inventory33.png
+java.io.FileNotFoundException: samsinvcrafting:textures/gui/inventory33.png*/
+		
+		this.mc.getTextureManager().bindTexture(new net.minecraft.util.ResourceLocation(ModInvCrafting.MODID, "textures/gui/inventory33.png"));
 	}
 }
