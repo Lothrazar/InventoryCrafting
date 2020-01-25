e489b54056d24ac0699034bc7aa0718772c5c513
lothrazar <samson.bassett@gmail.com>
2020-01-25 12:10:08 -0800

    modid double fixed

--- src/main/java/com/lothrazar/invcrafting/ModInvCrafting.java
+++ src/main/java/com/lothrazar/invcrafting/ModInvCrafting.java
@@ -1,14 +1,6 @@
 package com.lothrazar.invcrafting;
 
-import net.minecraft.nbt.NBTBase;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.Capability.IStorage;
-import net.minecraftforge.common.capabilities.CapabilityManager;
-import net.minecraftforge.common.capabilities.ICapabilityProvider;
-import net.minecraftforge.common.capabilities.ICapabilitySerializable;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -17,7 +9,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = ModInvCrafting.MODID, useMetadata = true, updateJSON = "https://raw.githubusercontent.com/LothrazarMinecraftMods/InventoryCrafting/master/update.json")
 public class ModInvCrafting {
 
-  public static final String MODID = "samsinvcrafting";
+  public static final String MODID = "invcrafting";
   @Instance(value = MODID)
   public static ModInvCrafting instance;
 
