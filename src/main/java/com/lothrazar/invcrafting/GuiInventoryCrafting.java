34c0b3c2003f95e23e16376fb21c6dd7f60ddf90
lothrazar <samson.bassett@gmail.com>
2020-03-27 23:22:50 -0700

    fix rendering entity. update to latest forge and add jei and baubles dependencies for testing

--- src/main/java/com/lothrazar/invcrafting/GuiInventoryCrafting.java
+++ src/main/java/com/lothrazar/invcrafting/GuiInventoryCrafting.java
@@ -1,5 +1,6 @@
 package com.lothrazar.invcrafting;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
@@ -20,7 +21,10 @@ public class GuiInventoryCrafting extends GuiInventory {
     //GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
     this.mc.getTextureManager().bindTexture(BACKGROUND);
     this.drawTexturedModalRect(this.guiLeft, this.guiTop, 0, 0, this.xSize, this.ySize);
-  }
+    int i = this.guiLeft;
+    int j = this.guiTop;
+    drawEntityOnScreen(i + 51, j + 75, 30, (float) (i + 51) - mouseX, (float) (j + 75 - 50) - mouseY, Minecraft.getMinecraft().player);
+     }
 
   @Override
   public void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
