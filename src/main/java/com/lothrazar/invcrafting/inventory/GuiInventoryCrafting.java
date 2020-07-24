13054b896c0291017793310c89c7185acb10b598
lothrazar <samson.bassett@gmail.com>
2020-07-23 21:34:22 -0700

    1.16 port

--- src/main/java/com/lothrazar/invcrafting/inventory/GuiInventoryCrafting.java
+++ src/main/java/com/lothrazar/invcrafting/inventory/GuiInventoryCrafting.java
@@ -1,6 +1,7 @@
 package com.lothrazar.invcrafting.inventory;
 
 import com.lothrazar.invcrafting.ModInvCrafting;
+import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.gui.screen.inventory.InventoryScreen;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ResourceLocation;
@@ -15,18 +16,18 @@ public class GuiInventoryCrafting extends InventoryScreen {
     this.container = ((p.openContainer instanceof ContainerPlayerCrafting) ? (ContainerPlayerCrafting) p.openContainer : null);
   }
 
-  @Override
-  protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
-    super.drawGuiContainerBackgroundLayer(partialTicks, mouseX, mouseY);
+  @Override //drawGuiContainerBackgroundLayer
+  protected void func_230450_a_(MatrixStack ms, float partialTicks, int mouseX, int mouseY) {
+    super.func_230450_a_(ms, partialTicks, mouseX, mouseY);
     this.minecraft.getTextureManager().bindTexture(BACKGROUND);
-    this.blit(this.guiLeft, this.guiTop, 0, 0, this.xSize, this.ySize);
+    this.blit(ms, this.guiLeft, this.guiTop, 0, 0, this.xSize, this.ySize);
     int i = this.guiLeft;
     int j = this.guiTop;
     drawEntityOnScreen(i + 51, j + 75, 30, (float) (i + 51) - mouseX, (float) (j + 75 - 50) - mouseY, this.minecraft.player);
   }
 
   @Override
-  public void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
+  public void func_230451_b_(MatrixStack ms, int mouseX, int mouseY) {
     // do not call super here to disable the title text rendering on screen
     //    super.drawGuiContainerForegroundLayer(mouseX, mouseY);
   }
